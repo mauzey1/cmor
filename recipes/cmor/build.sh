1473f9f12d4dbae4bb117d5d84a23c5d502d37f7
mauzey1 <mauzey1@llnl.gov>
2019-07-09 14:54:23 -0700

    Change order of CFLAGS

--- recipes/cmor/build.sh
+++ recipes/cmor/build.sh
@@ -13,7 +13,7 @@ fi
 if [[ `uname` == "Linux" ]]; then
     export LDSHARED_FLAGS="-shared -pthread"
 else
-    export CFLAGS="-Wl -syslibroot -isysroot ${CFLAGS}"
+    export CFLAGS="${CFLAGS} -Wl,-syslibroot / -isysroot"
     export LDSHARED_FLAGS="-bundle -undefined dynamic_lookup"
 fi
 
